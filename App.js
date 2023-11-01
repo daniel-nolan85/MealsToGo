466a5efd4171b71283e530afc29cc07ac3d21a6e
daniel-nolan85 <danielnolan85@yahoo.com>
2023-11-01 05:23:27 -0700

    individual card component

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { ThemeProvider } from 'styled-components/native';
 import { RestaurantsScreen } from './src/features/restaurants/screens/restaurants.screen';
+import { theme } from './src/infrastructure/theme';
+import {
+  useFonts as UseOswald,
+  Oswald_400Regular,
+} from '@expo-google-fonts/oswald';
+import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
 
 export default function App() {
+  const [oswaldLoaded] = UseOswald({ Oswald_400Regular });
+  const [latoLoaded] = useLato({ Lato_400Regular });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
   return (
     <>
-      <RestaurantsScreen />
+      <ThemeProvider theme={theme}>
+        <RestaurantsScreen />
+      </ThemeProvider>
       <ExpoStatusBar style='auto' />
     </>
   );
