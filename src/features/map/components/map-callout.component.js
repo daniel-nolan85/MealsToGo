bcdaf6e5ef8188d610ca7de068a880998373aa70
daniel-nolan85 <danielnolan85@yahoo.com>
2023-11-02 10:22:00 -0700

    maps

---
+++ src/features/map/components/map-callout.component.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native';
+import { CompactRestaurantInfo } from '../../../components/restaurant/compact-restaurant-info.component';
+
+const MyText = styled.Text``;
+
+export const MapCallout = ({ restaurant }) => (
+  <CompactRestaurantInfo restaurant={restaurant} />
+);
